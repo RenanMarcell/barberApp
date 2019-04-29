6bf1c5d6f93c7648d917d13efafd36e778044956
renan <renan.vieira@luizalabs.com>
2019-04-29 01:19:58 -0300

    add app without improvements

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const multerConfig = require('./config/multer');
+const upload = require('multer')(multerConfig);
+const AppointmentController = require('./app/controllers/AppointmentController');
+const AvailableController = require('./app/controllers/AvailableController');
+const DashboardController = require('./app/controllers/DashboardController');
+const FileController = require('./app/controllers/FileController');
+const SessionController = require('./app/controllers/SessionController');
+const UserController = require('./app/controllers/UserController');
+const authMiddleware = require('./app/middlewares/auth');
+const guestMiddleware = require('./app/middlewares/guest');
+
+const routes = express.Router();
+
+routes.use((req, res, next) => {
+	res.locals.flashSuccess = req.flash('success');
+	res.locals.flashError = req.flash('error');
+	return next();
+})
+routes.use('/app', authMiddleware);
+
+routes.get('/', guestMiddleware, SessionController.create);
+routes.get('/signup', guestMiddleware, UserController.create);
+routes.get('/files/:file', FileController.show);
+routes.get('/app/dashboard', DashboardController.index);
+routes.get('/app/logout', SessionController.destroy);
+routes.get('/app/appointments/new/:provider', AppointmentController.create);
+routes.get('/app/available/:provider', AvailableController.index);
+
+routes.post('/signup', upload.single('avatar'), UserController.store);
+routes.post('/signin', SessionController.store);
+routes.post('/app/appointments/new/:provider', AppointmentController.store);
+
+module.exports = routes;
